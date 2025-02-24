255ea876ebdec2f767f1bd5642edc333f88fae0f
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-24 14:04:23 -0300

    refactor(settings): standardize string quotes and improve formatting in settings configuration

--- src/app/infrastructure/config/settings.py
+++ src/app/infrastructure/config/settings.py
@@ -3,11 +3,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env', env_file_encoding='utf-8', case_sensitive=True, extra="allow"
+        env_file='.env',
+        env_file_encoding='utf-8',
+        case_sensitive=True,
+        extra='allow',
     )
 
     DATABASE_URL: str
-    API_PREFIX: str = "/v1"
+    API_PREFIX: str = '/v1'
 
 
 settings: Settings = Settings()
