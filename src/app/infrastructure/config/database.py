9672d11b0b84ee12e13423808dba308b8b1380bb
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-24 14:20:37 -0300

    refactor(database): remove unused init function from database configuration

--- src/app/infrastructure/config/database.py
+++ src/app/infrastructure/config/database.py
@@ -1,17 +1,11 @@
 from collections.abc import AsyncIterator
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.infrastructure.config.settings import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
-
-
-async def init() -> None:
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
 
 
 async def get_session() -> AsyncIterator[AsyncSession]:
