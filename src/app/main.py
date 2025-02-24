f6ba5f0b2bd4b37417181bd11063934a5adfbe6b
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-24 14:20:43 -0300

    refactor(main): remove lifespan context manager and related imports from main application file

--- src/app/main.py
+++ src/app/main.py
@@ -1,21 +1,10 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
-from app.infrastructure.config.database import init
 from app.infrastructure.config.settings import settings
 from app.presentation.controllers.user_controller import router as user_router
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init()
-    yield
-
-
 app = FastAPI(
     title='FastAPI Clean Architecture',
-    lifespan=lifespan,
 )
 
 app.include_router(
