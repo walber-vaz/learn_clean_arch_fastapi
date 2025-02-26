3a5ec2e6f7d3d6dd8ec56fa073d078d9a44d3a89
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-26 11:24:15 -0300

    feat(tests): add unit tests for user retrieval and listing in SqlModelUserRepository

--- tests/unit/infrastructure/repositories/test_sqlmodel_user_repository.py
+++ tests/unit/infrastructure/repositories/test_sqlmodel_user_repository.py
@@ -31,3 +31,47 @@ async def test_create_user(db_session):
     assert found_user.id == user.id
     assert found_user.email == user.email
     assert found_user.name == user.name
+
+
+@pytest.mark.asyncio
+async def test_find_by_email(db_session):
+    repository = SqlModelUserRepository(db_session)
+    user = User(
+        email=user_mock.email,
+        password=user_mock.password,
+        name=user_mock.name,
+        id=user_mock.id,
+    )
+
+    await repository.create(user)
+
+    found_user = await repository.find_by_email(user_mock.email)
+
+    assert found_user is not None
+    assert found_user.id == user.id
+    assert found_user.email == user.email
+    assert found_user.name == user.name
+
+
+@pytest.mark.asyncio
+async def test_list_users(db_session):
+    repository = SqlModelUserRepository(db_session)
+    user = User(
+        email=user_mock.email,
+        password=user_mock.password,
+        name=user_mock.name,
+        id=user_mock.id,
+    )
+
+    await repository.create(user)
+
+    users = await repository.index(page=1, page_size=10)
+
+    assert len(users) == 1
+    assert users[0].id == user.id
+    assert users[0].email == user.email
+    assert users[0].name == user.name
+
+    users = await repository.index(page=2, page_size=10)
+
+    assert len(users) == 0
