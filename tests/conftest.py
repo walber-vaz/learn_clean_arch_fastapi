cfd2e3d2252d4e3ccd52fef579465f480b79fb86
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-26 10:21:03 -0300

    fix(tests): update pytest configuration and refactor test fixtures for improved session management

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
-import asyncio
 from typing import AsyncGenerator
 
 import pytest_asyncio
 from fastapi.testclient import TestClient
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.pool import NullPool
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.domain import entities
 from app.infrastructure.config.database import get_session
@@ -21,57 +20,37 @@ test_engine = create_async_engine(
     echo=False,
     future=True,
     poolclass=NullPool,
-    connect_args={'check_same_thread': False},
 )
 
-test_async_session = sessionmaker(
+test_async_session = async_sessionmaker(
     test_engine, class_=AsyncSession, expire_on_commit=False
 )
 
 
 @pytest_asyncio.fixture(scope='session')
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
-
-
-@pytest_asyncio.fixture(scope='function')
-async def setup_database():
+async def create_tables():
     async with test_engine.begin() as conn:
-        await conn.run_sync(
-            entities.SQLModel.metadata.create_all, bind=test_engine
-        )
-
-    yield
-
-    async with test_engine.begin() as conn:
-        await conn.run_sync(
-            entities.SQLModel.metadata.drop_all, bind=test_engine
-        )
-
-    await test_engine.dispose()
+        await conn.run_sync(entities.SQLModel.metadata.drop_all)
+        await conn.run_sync(entities.SQLModel.metadata.create_all)
 
 
-@pytest_asyncio.fixture(scope='function')
-async def session(
-    setup_database,
-) -> AsyncGenerator[AsyncSession, None]:
-    async with test_async_session() as session:
-        try:
-            yield session
-        finally:
-            await session.rollback()
-            await session.close()
+@pytest_asyncio.fixture
+async def db_session(create_tables) -> AsyncGenerator[AsyncSession, None]:
+    connection = await test_engine.connect()
+    transaction = await connection.begin()
+    session = test_async_session(bind=connection)
+    try:
+        yield session
+    finally:
+        await session.close()
+        await transaction.rollback()
+        await connection.close()
 
 
-@pytest_asyncio.fixture(scope='function')
-async def client(session):
-    async def override_get_session():
-        try:
-            yield session
-        finally:
-            pass
+@pytest_asyncio.fixture
+async def client(db_session) -> AsyncGenerator[TestClient, None]:
+    async def override_get_session() -> AsyncGenerator[AsyncSession, None]:
+        yield db_session
 
     app.dependency_overrides[get_session] = override_get_session
 
