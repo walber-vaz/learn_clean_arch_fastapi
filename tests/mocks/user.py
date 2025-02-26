f35e0f08a8cdd6b0c9edaf17a0352ac3cdc4febd
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-26 16:48:47 -0300

    feat(tests): enhance user mock data with Faker for improved test reliability

--- tests/mocks/user.py
+++ tests/mocks/user.py
@@ -1,10 +1,20 @@
 from dataclasses import dataclass
 from uuid import UUID, uuid4
 
+from faker import Faker
+
+faker = Faker('pt_BR')
+
 
 @dataclass
 class User:
-    name: str = 'Test User'
-    email: str = 'email@email.com'
-    password: str = 'Password1!'
+    name: str = faker.name()
+    email: str = faker.email()
+    password: str = faker.password(
+        length=8,
+        special_chars=True,
+        digits=True,
+        upper_case=True,
+        lower_case=True,
+    )
     id: UUID = uuid4()
