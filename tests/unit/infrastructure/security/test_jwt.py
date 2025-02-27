86a0e142f4588010c3337ed5b38255b9c14d3b27
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-27 09:53:45 -0300

    test(jwt): add unit tests for token creation and decoding functions

---
+++ tests/unit/infrastructure/security/test_jwt.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+from time import sleep
+from zoneinfo import ZoneInfo
+
+import pytest
+from faker import Faker
+
+from app.infrastructure.security.jwt import (
+    TokenPayload,
+    create_access_token,
+    decode_access_token,
+)
+
+fake = Faker('pt_BR')
+
+
+@pytest.mark.order(1)
+def test_create_access_token():
+    sub = fake.uuid4()
+    token = create_access_token(sub)
+
+    assert token is not None
+    assert isinstance(token, str)
+
+
+@pytest.mark.order(2)
+def test_decode_token_valid():
+    sub = fake.uuid4()
+    token = create_access_token(sub)
+
+    decoded = decode_access_token(token)
+
+    assert decoded is not None
+    assert decoded.sub == sub
+    assert decoded.exp is not None
+    assert decoded.exp > datetime.now(ZoneInfo('America/Sao_Paulo'))
+    assert isinstance(decoded.exp, datetime)
+    assert isinstance(decoded, TokenPayload)
+
+
+@pytest.mark.order(3)
+def test_decode_token_expired():
+    subject = fake.uuid4()
+    expires = timedelta(seconds=1)
+    token = create_access_token(subject=subject, expires_delta=expires)
+
+    sleep(2)
+
+    with pytest.raises(ValueError, match='Could not validate credentials'):
+        decode_access_token(token)
+
+
+@pytest.mark.order(4)
+def test_decode_token_invalid():
+    invalid_token = fake.sha256()
+
+    with pytest.raises(ValueError, match='Could not validate credentials'):
+        decode_access_token(invalid_token)
