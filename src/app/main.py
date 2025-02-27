3eb84dbbf14536aae3d2e5650f685ea63439e70b
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-02-27 09:21:19 -0300

    feat(auth): add authentication router to main application

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.infrastructure.config.settings import settings
+from app.presentation.controllers.auth_controller import router as auth_router
 from app.presentation.controllers.user_controller import router as user_router
 
 app = FastAPI(
@@ -9,4 +10,7 @@ app = FastAPI(
 
 app.include_router(
     user_router, prefix=f'{settings.API_PREFIX}/users', tags=['users']
+)
+app.include_router(
+    auth_router, prefix=f'{settings.API_PREFIX}/auth', tags=['auth']
 )
